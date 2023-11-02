c95dab4c0a95d4e7e56460bccc22132551f59bd2
Alina Nosovets <alinanosovets6849@gmail.com>
2023-11-02 16:44:52 +0100

    feat: add react router

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,22 @@ import './App.css';
 import { ErrorBoundary } from './components/errorBoundary/ErrorBoundary';
 import { Home } from './pages/Home/Home';
 
+//react=router imports
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 export const App = () => {
   const onResetError = (): void => {
     console.clear();
   };
 
   return (
-    <ErrorBoundary onResetError={onResetError}>
-      <Home />
-    </ErrorBoundary>
+    <Router>
+      <ErrorBoundary onResetError={onResetError}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </ErrorBoundary>
+    </Router>
   );
 };
 
