f665249d1e379e4bbb2540c7b5efa73e5daf2a03
Alina Nosovets <alinanosovets6849@gmail.com>
2023-11-02 16:26:23 +0100

    refactor: remove redundant code from App.tsx, change class component to functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
 import './App.css';
-import { Component } from 'react';
 import { ErrorBoundary } from './components/errorBoundary/ErrorBoundary';
 import { Home } from './pages/Home/Home';
 
-interface PokemonItem {
-  name: string;
-  description: string;
-  image: string;
-}
-interface AppState {
-  searchTerm: string;
-  pokemons: PokemonItem[];
-  causeRenderError: boolean;
-  pokemonError: boolean;
-  pokemonsError: boolean;
-  loading: boolean;
-}
-
-export class App extends Component<unknown, AppState> {
-  onResetError = (): void => {
-    this.setState({ causeRenderError: false });
+export const App = () => {
+  const onResetError = (): void => {
     console.clear();
   };
 
-  render() {
-    return (
-      <ErrorBoundary onResetError={this.onResetError}>
-        <Home />
-      </ErrorBoundary>
-    );
-  }
-}
+  return (
+    <ErrorBoundary onResetError={onResetError}>
+      <Home />
+    </ErrorBoundary>
+  );
+};
 
 export default App;
