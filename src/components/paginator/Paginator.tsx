d45223e21153be5a91697f1a0c7f90a2e03d4ab4
Alina Nosovets <alinanosovets6849@gmail.com>
2023-11-02 19:21:36 +0100

    feat: update styles for pagination and limit sections

--- src/components/paginator/Paginator.tsx
+++ src/components/paginator/Paginator.tsx
@@ -1,4 +1,5 @@
 import './paginator.styles.css';
+import { useState } from 'react';
 
 interface PaginatorProps {
   page: number;
@@ -8,8 +9,12 @@ interface PaginatorProps {
   onLimitChange: (newLimit: number) => void;
 }
 
-export const Paginator = ({ page, limit, totalItems, onPageChange, onLimitChange }: PaginatorProps) => {
-  const totalPages = Math.ceil(totalItems / limit);
+export const Paginator = ({ page, limit, onPageChange, onLimitChange }: PaginatorProps) => {
+  const MAX_ITEMS_PER_PAGE = 100;
+  const [inputValue, setInputValue] = useState<string>(String(limit));
+  const [error, setError] = useState<string | null>(null);
+
+  const totalPages = Math.ceil(MAX_ITEMS_PER_PAGE / limit);
 
   const handlePrevious = () => {
     if (page > 1) onPageChange(page - 1);
@@ -19,32 +24,51 @@ export const Paginator = ({ page, limit, totalItems, onPageChange, onLimitChange
     if (page < totalPages) onPageChange(page + 1);
   };
 
+  const handleSubmit = () => {
+    const newLimit = Number(inputValue);
+
+    if (isNaN(newLimit) || !Number.isInteger(newLimit)) {
+      setError('Please enter a valid number.');
+      return;
+    }
+
+    if (newLimit < 1 || newLimit > MAX_ITEMS_PER_PAGE) {
+      setError(`Please enter a number between 1 and ${MAX_ITEMS_PER_PAGE}`);
+      return;
+    }
+
+    setError(null);
+    onLimitChange(newLimit);
+    onPageChange(1);
+  };
+
   return (
     <div className="paginator">
-      <button onClick={handlePrevious} disabled={page === 1}>
-        Previous
-      </button>
-      <span>
-        Page {page} of {totalPages}
-      </span>
-      <button onClick={handleNext} disabled={page === totalPages}>
-        Next
-      </button>
-
-      <div className="limit-selector">
-        <label>Items per page:</label>
-        <select
-          value={limit}
-          onChange={(e) => {
-            onLimitChange(Number(e.target.value));
-            onPageChange(1);
-          }}
-        >
-          <option value="10">10</option>
-          <option value="20">20</option>
-          <option value="50">50</option>
-        </select>
-      </div>
+      <section className="pagination-section">
+        <button onClick={handlePrevious} disabled={page === 1}>
+          Previous
+        </button>
+        <span>
+          Page {page} of {totalPages}
+        </span>
+        <button onClick={handleNext} disabled={page === totalPages}>
+          Next
+        </button>
+      </section>
+      <section className="limit-section">
+        <div className="limit-selector">
+          <label>Items per page:</label>
+          <input
+            className="limit-input"
+            placeholder="enter the number from 1 to 100"
+            type="text"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <button onClick={handleSubmit}>Submit</button>
+          {error && <div className="error-message">{error}</div>}
+        </div>
+      </section>
     </div>
   );
 };
